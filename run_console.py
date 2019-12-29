a507aaeefdd0f2c8d2ef83f0e7856c4fc61d2afd
fan.yao <yaofan29597@163.com>
2019-12-29 02:14:29 -0500

    redesign reward signal

--- run_console.py
+++ run_console.py
@@ -9,15 +9,15 @@ import os
 import gc
 from tqdm import tqdm
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-problem = KCut_DGL(k=5, m=6, adjacent_reserve=20, hidden_dim=16, random_init_label=True, a=1)
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+problem = KCut_DGL(k=5, m=6, adjacent_reserve=20, hidden_dim=32, random_init_label=True, a=1)
 alg = DQN(problem, gamma=0.9, eps=0.1, lr=.02, replay_buffer_max_size=10, cuda_flag=True)
 
 def run_dqn():
     for i in tqdm(range(100)):
         T1 = time.time()
         # TODO memory usage :: episode_len * num_episodes * hidden_dim
-        log = alg.train_dqn(batch_size=64, grad_accum=10, num_episodes=1, episode_len=500, gcn_step=10, q_step=1, ddqn=False)
+        log = alg.train_dqn(batch_size=64, grad_accum=20, num_episodes=1, episode_len=500, gcn_step=20, q_step=1, ddqn=False)
         if i % 3 == 0:
             alg.update_target_net()
         T2 = time.time()
@@ -26,14 +26,14 @@ def run_dqn():
 
 run_dqn()
 
-def test_model(step=10):
-    test_problem = KCut_DGL(k=5, m=6, adjacent_reserve=20, hidden_dim=16, random_init_label=True, a=1)
+def test_model(step=100):
+    test_problem = KCut_DGL(k=5, m=6, adjacent_reserve=20, hidden_dim=32, random_init_label=True, a=1)
     n = test_problem.k * test_problem.m
     g = to_cuda(test_problem.g)
     ep = EpisodeHistory(g, max_episode_len=step)
 
     for i in range(step):
-        S_a_encoding, h, Q_sa = alg.model(g, step=10)
+        S_a_encoding, h, Q_sa = alg.model(g, step=20)
         best_action = Q_sa.argmax()
         swap_i, swap_j = best_action / n, best_action - best_action / n * n
         state, reward = test_problem.step((swap_i, swap_j))
